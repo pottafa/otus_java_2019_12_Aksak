8925f0137435c080a13c3699c6057e9a020aaf43
Aleksandr <pottafa@gmail.com>
2020-02-18 03:29:06 +0300

    Exception catching minor update

--- hw05-TestingFramework/src/main/java/ru/otus/homework/TestClass.java
+++ hw05-TestingFramework/src/main/java/ru/otus/homework/TestClass.java
@@ -3,37 +3,40 @@ package ru.otus.homework;
 import ru.otus.homework.testingFramework.annotations.*;
 
 public class TestClass {
-    @BeforeAll
-    public static void globalSetOup() {
-        System.out.println("BeforeAll method starts");
-    }
+  @BeforeAll
+  public static void globalSetUp() {
+    System.out.println("BeforeAll method starts");
+    //   throw new IndexOutOfBoundsException();
+  }
 
-    @Before
-    public void setUp() {
-        System.out.println("Before method starts");
-    }
+  @Before
+  public void setUp() {
+    System.out.println("Before method starts");
+    //   throw new IndexOutOfBoundsException();
+  }
 
-    @Test
-    public void test1() {
-        System.out.println("Test1 starts");
-    }
+  @Test
+  public void test1() {
+    System.out.println("Test1 starts");
+  }
 
-    @Test
-    public void test2() {
-        System.out.println("Tests 2 starts");
-        Object object = null;
-        System.out.println(object.toString());
-    }
+  @Test
+  public void test2() {
+    System.out.println("Tests 2 starts");
+    Object object = null;
+    //      System.out.println(object.toString());
+  }
 
-    @After
-    public void tearDown() {
-        System.out.println("After method starts");
-        Object object = null;
-    }
+  @After
+  public void tearDown() {
+    System.out.println("After method starts");
+    //  throw new IndexOutOfBoundsException();
+  }
 
-    @AfterAll
-    public static void globalTearDown() {
-        System.out.println("AfterAll method starts");
-    }
+  @AfterAll
+  public static void globalTearDown() {
+    System.out.println("AfterAll method starts");
+    //  throw new UnsupportedOperationException();
+  }
 
 }
--- hw05-TestingFramework/src/main/java/ru/otus/homework/testingFramework/TestsHandler.java
+++ hw05-TestingFramework/src/main/java/ru/otus/homework/testingFramework/TestsHandler.java
@@ -1,58 +1,92 @@
 package ru.otus.homework.testingFramework;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.*;
 
 
 class TestsHandler {
-    private Method beforeAll = null;
-    private Method before = null;
-    private List<Method> tests = new ArrayList<>();
-    private Method after = null;
-    private Method afterAll = null;
+  private Method beforeAll = null;
+  private Method before = null;
+  private List<Method> tests = new ArrayList<>();
+  private Method after = null;
+  private Method afterAll = null;
 
-    void setAfterAll(Method afterAll) throws TestingException {
-        if (this.afterAll != null) throw new TestingException("two or more AfterAll methods detected");
-        this.afterAll = afterAll;
-    }
+  void setAfterAll(Method afterAll) throws TestingException {
+    if (this.afterAll != null) throw new TestingException("two or more AfterAll methods detected");
+    this.afterAll = afterAll;
+  }
 
-    void setBeforeAll(Method beforeAll) throws TestingException {
-        if (this.beforeAll != null) throw new TestingException("two or more BeforeAll methods detected");
-        this.beforeAll = beforeAll;
-    }
+  void setBeforeAll(Method beforeAll) throws TestingException {
+    if (this.beforeAll != null) throw new TestingException("two or more BeforeAll methods detected");
+    this.beforeAll = beforeAll;
+  }
 
-    void setBefore(Method before) throws TestingException {
-        if (this.before != null) throw new TestingException("two or more Before methods detected");
-        this.before = before;
-    }
+  void setBefore(Method before) throws TestingException {
+    if (this.before != null) throw new TestingException("two or more Before methods detected");
+    this.before = before;
+  }
 
-    void setAfter(Method after) throws TestingException {
-        if (this.after != null) throw new TestingException("two or more After methods detected");
-        this.after = after;
-    }
+  void setAfter(Method after) throws TestingException {
+    if (this.after != null) throw new TestingException("two or more After methods detected");
+    this.after = after;
+  }
 
-    void addTest(Method test) {
-        tests.add(test);
-    }
+  void addTest(Method test) {
+    tests.add(test);
+  }
 
-    void execute(Constructor constructor, Map<String, Integer> statistics) throws Exception {
-        Collections.shuffle(tests);
-        if (tests.size() == 0) throw new TestingException("No methods to test in " + constructor.getName());
-        if (beforeAll != null) beforeAll.invoke(null);
-        for (Method test : tests) {
-            var classObject = constructor.newInstance();
-            if (before != null) before.invoke(classObject);
-            try {
-                test.invoke(classObject);
-                statistics.merge("Tests passed: ", 1, Integer::sum);
-            } catch (Exception e) {
-                statistics.merge("Tests failed: ", 1, Integer::sum);
-                e.getCause().printStackTrace();
-            }
-            if (after != null) after.invoke(classObject);
+  void execute(Constructor constructor, Map<String, Integer> statistics) throws Exception {
+    Collections.shuffle(tests);
+    if (tests.size() == 0) throw new TestingException("No methods to test in " + constructor.getName());
+    try {
+      invokeBeforeAll();
+      for (Method test : tests) {
+        var classObject = constructor.newInstance();
+        try {
+          invokeBefore(classObject, before);
+          test.invoke(classObject);
+        } catch (Exception e) {
+          statistics.merge("Tests failed: ", 1, Integer::sum);
+          e.getCause().printStackTrace();
+          continue;
+        } finally {
+          try {
+            invokeAfter(classObject);
+          } catch (Exception e) {
+            statistics.merge("Tests failed: ", 1, Integer::sum);
+            e.getCause().printStackTrace();
+            continue;
+          }
         }
-        if (afterAll != null) afterAll.invoke(null);
-        statistics.put("Total number of tests: ", tests.size());
+        statistics.merge("Tests passed: ", 1, Integer::sum);
+      }
+    } catch (Exception e) {
+      e.getCause().printStackTrace();
+    } finally {
+      statistics.put("Total number of tests: ", tests.size());
+      invokeAfterAll();
     }
+  }
+
+  private void invokeBefore(Object classObject, Method before) throws IllegalAccessException, InvocationTargetException {
+    if (before != null) before.invoke(classObject);
+  }
+
+  private void invokeAfter(Object classObject) throws IllegalAccessException, InvocationTargetException {
+    if (after != null) after.invoke(classObject);
+  }
+
+  private void invokeAfterAll() {
+    try {
+      if (afterAll != null) afterAll.invoke(null);
+    } catch (Exception e) {
+      e.getCause().printStackTrace();
+    }
+  }
+
+  private void invokeBeforeAll() throws IllegalAccessException, InvocationTargetException {
+    if (beforeAll != null) beforeAll.invoke(null);
+  }
 }
