1ea36e2293857b3508e010e83fc636fe04f4330e
Aleksandr <pottafa@gmail.com>
2020-04-26 04:33:35 +0300

    Fixed: syncCount() method structure

--- hw14-SequenceOfNumbers/src/main/java/ru/otus/homework/SynchronizedSequence.java
+++ hw14-SequenceOfNumbers/src/main/java/ru/otus/homework/SynchronizedSequence.java
@@ -4,8 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class SynchronizedSequence {
+  private static final int SEQUENCE_NUMBER = 10;
   private static Logger logger = LoggerFactory.getLogger(SynchronizedSequence.class);
-  private volatile Thread currentThread;
+  private Thread lastThread;
 
   public static void main(String[] args) {
     SynchronizedSequence sequence = new SynchronizedSequence();
@@ -13,33 +14,33 @@ public class SynchronizedSequence {
   }
 
   public void sequence() {
-    for (int i = 1; i <= 10; i++) {
+    for (int i = 1; i <= SEQUENCE_NUMBER; i++) {
       syncCount(i);
     }
-    for (int i = 9; i > 0; i--) {
+    for (int i = SEQUENCE_NUMBER - 1; i > 0; i--) {
       syncCount(i);
     }
-    currentThread.interrupt();
   }
 
   private synchronized void syncCount(int count) {
     try {
-      currentThread = Thread.currentThread();
-      notify();
-      logger.info(String.valueOf(count));
-      while (currentThread.equals(Thread.currentThread())) {
+      while (lastThread.equals(Thread.currentThread())) {
         wait();
       }
+      logger.info(String.valueOf(count));
+      lastThread = Thread.currentThread();
       Thread.sleep(100);
+      notifyAll();
     } catch (InterruptedException ex) {
-      logger.info(currentThread.getName() + " has been interrupted");
-      currentThread.interrupt();
+      logger.info(lastThread.getName() + " has been interrupted");
+      lastThread.interrupt();
     }
   }
 
 
   public void start() {
     Thread thread1 = new Thread(this::sequence);
+    lastThread = thread1;
     Thread thread2 = new Thread(this::sequence);
 
     thread1.start();
